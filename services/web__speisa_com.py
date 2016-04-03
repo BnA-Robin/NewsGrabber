d6e0461d3925a9fa98e4214838332ca064e3aeb2
BnA-Robin <yorick@0xf.nl>
2016-04-03 12:42:27 +0200

    Added speisa.com service

---
+++ services/web__speisa_com.py
@@ -0,0 +1,9 @@
+refresh = 6
+version = 20160403.01
+
+urls = ['http://speisa.com/',
+	'http://speisa.com/modules/Most-Popular-Articles/',
+	'http://speisa.com/modules/last_100/']
+regex = [r'^https?:\/\/[^\/]*speisa\.com']
+videoregex = []
+liveregex = []
